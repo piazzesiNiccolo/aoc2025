acdceb81c9350ae417d00a43942e20a1a8e033ed
piazzesiNiccolo <npiazzesi@gmail.com>
2025-12-30 16:48:53 +0100

    Day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,10 +1,10 @@
 fun main() {
   fun part1(input: List<String>): Long {
+    val add = { i1: Long, i2: Long -> i2 + i1 }
+    val mult = { i1: Long, i2: Long -> i2 * i1 }
     val opsStrings = input.last().split(" ").filter { it.isNotEmpty() }
     val count = LongArray(opsStrings.size) { 0L }
     val ops = arrayOfNulls<(Long, Long) -> Long>(opsStrings.size)
-    val add = { i1: Long, i2: Long -> i2 + i1 }
-    val mult = { i1: Long, i2: Long -> i2 * i1 }
     for ((i, op) in opsStrings.withIndex()) {
       if (op == "*") {
         ops[i] = mult
@@ -19,12 +19,44 @@ fun main() {
         count[i] = op(count[i], n.toLong())
       }
     }
-
     return count.sum()
   }
 
   fun part2(input: List<String>): Long {
+
+    val add = List<Long>::sum
+    val mult = { input: List<Long> -> input.fold(1L) { a, b -> a * b } }
     var count = 0L
+    val r = Regex("([+*])( *)")
+    val lines = input.subList(0, input.size - 1)
+    val opString = input.last()
+    var offset = 0
+    val numStrs = mutableListOf<Pair<List<String>, (List<Long>) -> Long>>()
+    while (offset < opString.length) {
+      val match = r.matchAt(opString, offset)!!
+      val strings =
+          lines.map {
+            val range =
+                if (match.range.start != match.range.endInclusive) match.range
+                else match.range.start..it.lastIndex
+            it.slice(range)
+          }
+      val op = if (match.groups[1]!!.value == "*") mult else add
+      numStrs.add(strings to op)
+      offset = match.range.last + 1
+    }
+    for ((strs, op) in numStrs) {
+      val zipped = (0 until strs.minOf { it.length }).map { i -> strs.map { it[i] } }
+      val numbers = mutableListOf<Long>()
+      for (digits in zipped) {
+        val n = digits.joinToString("").trim()
+        if (n.isNotEmpty()) {
+          numbers.add(digits.joinToString("").trim().toLong())
+        }
+      }
+      count += op(numbers)
+    }
+
     return count
   }
 
@@ -33,6 +65,6 @@ fun main() {
       part1 = ::part1,
       part2 = ::part2,
       expectedPart1 = 4277556L,
-      expectedPart2 = 0L,
+      expectedPart2 = 3263827L,
   )
 }
